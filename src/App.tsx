b5c0ec84bd90f8dd23e7fec4d19e18b16a91c56b
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-05-10 17:23:43 -0600

    Added all routes to App file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Route, Routes, useNavigate } from 'react-router-dom';
+import { VStack, Box, useColorMode } from '@chakra-ui/react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Resume from './pages/Resume';
 
 function App() {
-	return <div>Hello</div>;
+	const navigate = useNavigate();
+
+	const { colorMode } = useColorMode();
+
+	const [mode, setMode] = useState(
+		localStorage.getItem('chakra-ui-color-mode')
+	);
+
+	useEffect(() => {
+		setMode(localStorage.getItem('chakra-ui-color-mode'));
+	}, [colorMode]);
+
+	return (
+		<Box
+			color={mode === 'dark' ? 'brand.lightGray' : 'brand.gray'}
+			bg={mode === 'dark' ? 'black' : '#fafafa'}
+			pb='20'
+		>
+			<Navbar navigation={navigate} />
+			<VStack minH='100vh' w='100%' m='auto' p={['5', '10']}>
+				<Routes>
+					<Route path='/' element={<Home />} />
+					<Route path='/Resume' element={<Resume />} />
+				</Routes>
+			</VStack>
+		</Box>
+	);
 }
 
 export default App;
