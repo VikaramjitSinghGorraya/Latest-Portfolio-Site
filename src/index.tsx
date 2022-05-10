530ac3570ee5112091268d60c884f4ff4d76ae96
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-05-10 17:15:53 -0600

    Added Chakra provider.

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ChakraProvider } from '@chakra-ui/react';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
+import { BrowserRouter } from 'react-router-dom';
+import customTheme from './theme/index';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(
 	document.getElementById('root') as HTMLElement
 );
+
+const queryClient = new QueryClient();
 root.render(
 	<React.StrictMode>
-		<ChakraProvider>
-			<App />
-		</ChakraProvider>
+		<ColorModeScript initialColorMode={customTheme.config.initialColorMode} />
+		<QueryClientProvider client={queryClient}>
+			<ChakraProvider theme={customTheme}>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</ChakraProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
