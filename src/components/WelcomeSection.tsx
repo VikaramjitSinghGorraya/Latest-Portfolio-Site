8189db77d40a85ae48fffc71aefffe949207e496
Vikaramjit Singh <vikaramjitsinghgorraya@gmail.com>
2022-11-21 10:52:37 -0700

    Adjusted the size of heading.

--- src/components/WelcomeSection.tsx
+++ src/components/WelcomeSection.tsx
@@ -61,7 +61,7 @@ const WelcomeSection = () => {
 					variant='smallTopHeading'
 					position='absolute'
 					top={['9.2vw', '60px']}
-					left={['6vw', '38px']}
+					left={['6vw', '30px']}
 				>
 					Hi, I am <span className='blueText'>Vikaramjit Singh</span>
 				</Heading>
